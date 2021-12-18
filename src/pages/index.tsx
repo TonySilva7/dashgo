62805c4ee2e09c555b82e5a121d715d671b3dfda
TonySilva7 <tony.deve7oper@gmail.com>
2021-12-18 19:46:41 -0300

    feat: create input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        flexDir="column"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" label="Email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+        <Button type="submit" mt={6} colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
