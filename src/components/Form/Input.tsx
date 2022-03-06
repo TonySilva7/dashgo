28b9056597d347e62e3c581bc38ff66462a9ae24
TonySilva7 <tony.deve7oper@gamil.com>
2022-03-06 11:12:16 -0300

    feat: validation native with reac-hook-form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,18 +1,25 @@
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { FieldError } from 'react-hook-form';
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+export const MyInput: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error, ...rest },
+  ref,
+) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && (
         <FormLabel htmlFor={name} id={name + label}>
           {label}
@@ -27,7 +34,12 @@ export function Input({ name, label, ...rest }: InputProps) {
         _hover={{ bgColor: 'gray.900' }}
         size="lg"
         {...rest}
+        ref={ref}
       />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(MyInput);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
-import { useState } from 'react';
 import { Input } from '../components/Form/Input';
-import InputMask from '../components/InputMask';
+// import InputMask from '../components/InputMask';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-export default function Home() {
-  const [card, setCard] = useState('');
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+export default function SignIn() {
+  // const [card, setCard] = useState('');
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+  console.log(errors);
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -15,16 +29,35 @@ export default function Home() {
         bg="gray.800"
         p="8"
         borderRadius={8}
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="Email" />
-          <Input name="password" label="Senha" type="password" />
-          <InputMask setCard={setCard} card={card} type="doc" />
+          <Input
+            name="email"
+            label="Email"
+            type="email"
+            {...register('email', { required: 'Email é obrigatório' })}
+            error={errors.email}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            {...register('password', { required: 'Senha é obrigatório' })}
+            error={errors.password}
+          />
+          {/* <InputMask setCard={setCard} card={card} type="doc" /> */}
         </Stack>
-        <Button type="submit" mt={6} colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          isLoading={formState.isSubmitting}
+          mt={6}
+          colorScheme="pink"
+          size="lg"
+        >
           Entrar
         </Button>
-        <Button
+        {/* <Button
           type="button"
           mt={6}
           colorScheme="blue"
@@ -32,7 +65,7 @@ export default function Home() {
           onClick={() => alert(String(card))}
         >
           Enviar
-        </Button>
+        </Button> */}
       </Flex>
     </Flex>
   );
